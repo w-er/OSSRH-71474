be8e6cfa063364a44768550aeec07daff1845f90
王林 <encodmail@126.com>
2021-09-16 17:02:08 +0800

    增加web跨域对象

--- wencoder-tools/src/main/java/com/wencoder/tools/ToolsConfig.java
+++ wencoder-tools/src/main/java/com/wencoder/tools/ToolsConfig.java
@@ -8,6 +8,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * 核心组件的入口配置类
@@ -42,4 +45,26 @@ public class ToolsConfig {
         return new ExceptionHandlerAdvice();
     }
 
+    /**
+     * 跨域
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public CorsFilter cors() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        /* 允许cookies跨域*/
+        config.setAllowCredentials(true);
+        /* 允许向该服务器提交请求的URI，*表示全部允许*/
+        // 携带cookie不能写*，可以写0
+        config.addAllowedOrigin("*");
+        /* 允许访问的头信息,*表示全部*/
+        config.addAllowedHeader("*");
+        /* 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了*/
+        config.setMaxAge(18000L);
+        /* 允许提交请求的方法，*表示全部允许*/
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
