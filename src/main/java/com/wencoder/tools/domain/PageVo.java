21ae119e74158e85e39bfcfc362b39665f7b0273
王林 <encodmail@126.com>
2021-11-05 20:46:54 +0800

    0.2

---
+++ src/main/java/com/wencoder/tools/domain/PageVo.java
@@ -0,0 +1,30 @@
+package com.wencoder.tools.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 分页结果
+ * <p>
+ * Created by 王林 on 2021-01-14 11:01:06
+ */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "分页结果", description = "分页结果")
+public class PageVo<T> implements Serializable {
+
+    private static final long serialVersionUID = -6868692026104831795L;
+
+    @ApiModelProperty(value = "总记录条", required = true, example = "99999")
+    private Long total;
+
+    @ApiModelProperty(position = 2, value = "分页列表")
+    private List<T> array;
+
+}
--- wencoder-tools/README.md
+++
@@ -1,9 +0,0 @@
-### 功能说明
-
-- 通用对象
-- 工具
-    - 时间处理工具 DateUtil
-    - 对象复制工具 CopyUtil
-- 异常处理
-- 异常公共错误码 ExceptionCode
-- Spring MVC 参数校验支持
